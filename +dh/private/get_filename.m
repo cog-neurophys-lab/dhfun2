9c5c5b98f34038cdde5b8cf7199019eea5d3aaa7
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-07-10 15:02:48 +0200

    Add H5ML.id to get_filename

--- +dh/private/get_filename.m
+++ +dh/private/get_filename.m
@@ -1,9 +1,11 @@
 function filename = get_filename(fid)
 
 if isinteger(fid)
-    filename = fopen(fid);
+    filename = (fid);
 elseif ischar(fid) || isstring(fid)
     filename = fid;
+elseif isa(fid, 'H5ML.id')
+    filename = H5F.get_name(fid);
 else
     error("Cannot determine filename from fid/filename");
 end
