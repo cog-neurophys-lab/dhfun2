e5e5fe29d8bd352f1160c6307fea291e07f9abba
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-07-12 13:56:51 +0200

    Add error checking to get_filename

--- +dh/private/get_filename.m
+++ +dh/private/get_filename.m
@@ -10,4 +10,16 @@ else
     error("Cannot determine filename from fid/filename");
 end
 
+if (hdfml('ishdf',filename) == 0)
+    error('File is not HDF');
+end
+
+if (hdfml('isfile',filename) == 0)
+    error('File does not exist');
+end
+
+if (hdfml('isreadonly',filename) == 1)
+    error('File is read-only');
+end
+
 end
