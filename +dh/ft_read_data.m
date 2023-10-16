3176d0746d7e7b09994301b34a2f419e7c65b76c
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-10-16 17:03:20 +0200

    Add reading of data into ft_read_data
    This is only supported for all channels having the same number
     of samples.

--- +dh/ft_read_data.m
+++ +dh/ft_read_data.m
@@ -1,5 +1,28 @@
 function data = ft_read_data(filename, hdr, begsample, endsample, chanindx)
 
+conts = dh.enumcont(filename);
+selectedConts = conts(chanindx);
 
+nSamples = zeros(size(conts));
+nChannels = 0;
+for iCont = 1:length(selectedConts)
+    [nSamples(iCont), nChanInCont] = dh.getcontsize(filename, conts(iCont));
+    nChannels = nChannels + nChanInCont;
+end
+
+if ~all(nSamples <= endsample-begsample+1)
+    error('Not all channels have enough samples');
+end
+
+data = zeros(length(nChannels), endsample-begsample+1);
+
+iChannel = 1;
+for iCont = 1:length(chanindx)
+    [time, ~] = dh.readcontindex(filename, selectedConts(iCont));
+    if length(time) > 1
+        warning('More than one data segment in CONT%d. This is not supported for Fieldtrip', selectedConts(iCont));
+    end
+    data(iChannel,:) = dh.readcont(filename, selectedConts(iCont), begsample, endsample);
+end
 
 end
