a5640c141823d38720c1235c1f0adb439fbba389
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-10-17 15:43:10 +0200

    Use new dh.ft_read_header in dh.ft_read_cont

--- +dh/ft_read_cont.m
+++ +dh/ft_read_cont.m
@@ -5,14 +5,6 @@ arguments
     blkid double {mustBeInteger,mustBePositive} = []
 end
 
-data = [];
-data.label = {};
-data.time = {};
-data.trial = {};
-data.sampleinfo = [];
-data.trialinfo = [];
-data.cfg = [];
-
 allContIds = dh.enumcont(filename);
 
 % take all cont blocks if blkid is not specified
@@ -20,64 +12,42 @@ if isempty(blkid)
     blkid = allContIds;
 end
 
-if any(~ismember(blkid, allContIds))
+% hdr (header information of the original dataset on disk)
+data.hdr = dh.ft_read_header(filename, blkid);
+
+blkid = data.hdr.includedContIds;
+
+if any(~ismember(blkid, data.hdr.includedContIds))
     error('dhfun2:ft_read_cont:InvalidBlockId', 'Invalid CONT block ID specified');
 end
 
-[nSamples, nChanInCont] = dh.getcontsize(filename, blkid);
-if ~all(nSamples == nSamples(1))
-    error('dhfun2:ft_read_cont:NonMatchingNumberOfSamples', 'Not all selected CONT blocks have the same number of samples');
-end
-nSamples = nSamples(1);
-nChannels = sum(nChanInCont);
-
-
-samplePeriod = dh.getcontsampleperiod(filename, blkid);
-if ~all(samplePeriod == samplePeriod(1))
-    error('dhfun2:ft_read_cont:NonMatchingSamplePeriods','Not all selected CONT blocks have the same sample period');
-end
-samplePeriod = samplePeriod(1);
-data.fsample = 1/samplePeriod*1E9;
+data.fsample = data.hdr.Fs;
 
 nIndex = dh.getcontindexsize(filename, blkid);
 if any(nIndex > 1)
-    warning('dhfun2:ft_read_cont:MultipleTrialsPerCont', 'File contains multiple trials per CONT block. This is not (yet) supported. All trials will be concatenated into one trial.');
+    warning('dhfun2:ft_read_cont:MultipleTrialsPerCont', ...
+        'File contains multiple trials per CONT block. This is not (yet) supported. All trials will be concatenated into one trial.');
 end
 
+% channel labels
+data.label = data.hdr.label;
 
-data.label = cell(1,nChannels);
-data.trial = {zeros(nChannels, nSamples)};
+% trial (load actual data)
+data.trial = {zeros(data.hdr.nChans, data.hdr.nSamples)};
+[~, nChanInCont] = dh.getcontsize(filename, blkid);
 
 channelBegin = 1;
 for iCont = 1:length(blkid)
     channelEnd = channelBegin + nChanInCont(iCont)-1;
     data.trial{1}(channelBegin:channelEnd,:) = dh.readcont(filename, blkid(iCont));
-    
-    for iChannel = 0:nChanInCont(iCont)-1
-        data.label{channelBegin+iChannel} = sprintf('CONT%d/%02d', blkid(iCont), iChannel);
-    end
     channelBegin = channelEnd+1;
 end
 
-% time of first sample
-tStart = zeros(1,length(blkid));
-for iCont = 1:length(blkid)
-    [time, offset] = dh.readcontindex(filename, blkid(iCont));
-    tStart(iCont) = time(1);
-end
-if ~all(tStart == tStart(1))
-    warning('dhfun2:ft_read_cont:NonMatchingStartTime', 'Not all selected CONT blocks have the same start time. The first start time will be used.');
-end
-tStart = tStart(1)/1e9;
-
 % time
-data.time = {(0:nSamples-1)*samplePeriod/1E9 + tStart};
+data.time = {(0:data.hdr.nSamples-1)*data.hdr.Fs + data.hdr.FirstTimeStamp};
 
 % sampleinfo: the begin and endsample of each trial relative to the recording on disk
-data.sampleinfo = [1, nSamples];
-
-% hdr (header information of the original dataset on disk)
-data.hdr = dh.ft_read_header(filename);
+data.sampleinfo = [1, data.hdr.nSamples];
 
 % cfg
 data.cfg = [];
@@ -90,8 +60,6 @@ data.cfg.tool = "dhfun2:ft_read_cont";
 data.cfg.dhfunVersion = dh.version();
 [data.cfg.previous.opnames, data.cfg.previous.opinfos] = dh.getoperationinfos(filename);
 
-% TODO:
-% - trialinfo
 
 % Check data if Fieldtrip is available on the path
 if exist('ft_defaults', 'file') == 2
