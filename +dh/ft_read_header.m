4e0130279f5bd6209f202dd3cee085571ffa5901
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-10-17 15:42:46 +0200

    Extend dh.ft_read_header to handle non-matchin properties
    - dh.ft_read_header now handles non-matching sample periods, first time
      stamps of CONT blocks
    - header includes the trialmap
    -  dh.ft_read_header can be given a list of CONT ids to include

--- +dh/ft_read_header.m
+++ +dh/ft_read_header.m
@@ -1,4 +1,4 @@
-function header = ft_read_header(filename)
+function header = ft_read_header(filename, conts)
 % This returns a header structure with the following fields
 %   header.Fs          = sampling frequency
 %   header.nChans      = number of channels
@@ -15,19 +15,25 @@ function header = ft_read_header(filename)
 %   header.TimeStampPerSample  number, represented in double precision
 %
 
+
+arguments
+    filename char
+    conts double {mustBeInteger} = []
+end
+
 header = [];
 
-%% TODO: handle selection of CONT blocks
-conts = dh.enumcont(filename);
+allContIds = dh.enumcont(filename);
+if isempty(conts)
+    conts = allContIds;
+end
 
 %% determine number of trials
-trialMapData = h5read(filename, '/TRIALMAP');
-header.nTrials = length(trialMapData.TrialNo);
-
+header.trialmap = h5read(filename, '/TRIALMAP');
+header.nTrials = length(header.trialmap.TrialNo);
 
 %% determine sampling frequency
 samplePeriods = dh.getcontsampleperiod(filename, conts);
-
 if ~all(samplePeriods(1) == samplePeriods)
 
     % select the most occuring sampling period
@@ -40,6 +46,28 @@ else
     header.Fs = 1/samplePeriods(1)*1E9;
 
 end
+
+%% determine first timestamp in data
+firstTimeStamp = zeros(1, length(conts));
+for iCont = 1:length(conts)
+    [time, offset] = dh.readcontindex(filename, conts(iCont));
+    firstTimeStamp(iCont) = time(1);
+end
+
+if ~all(firstTimeStamp(1) == firstTimeStamp)
+    % select the most occuring firstTimeStamp
+    [mostCommonFirstTimeStamp, iSelectedFirstTimeStamps] = pick_most_common_element(firstTimeStamp);
+    warning("dhfun2:ft_read_header:NonMatchingFirstTimeStamps", ...
+        "First time stamp of CONT blocks in DH5 file don't match.\n\tExcluding CONT blocks:\n\t %s", num2str(conts(firstTimeStamp(1) ~= firstTimeStamp)))
+    header.FirstTimeStamp = mostCommonFirstTimeStamp;
+    conts = conts(iSelectedFirstTimeStamps);
+
+
+else
+    header.FirstTimeStamp = firstTimeStamp(1);
+end
+
+
 % header.TimeStampPerSample = samplePeriods;
 % header.FirstTimeStamp =
 
@@ -48,10 +76,9 @@ end
 
 % check if its the same across CONT blocks
 if ~all(nSamples(1) == nSamples)
-
     % select the most common number of samples
     [mostCommonNSamples, iSelectedNSamples] = pick_most_common_element(nSamples);
-    warning("dhfun2:ft_read_header:NonMatchingNSamples", "Number of samples in CONT blocks of DH5 file don't match.\n\tExcluding CONT blocks:\n\t %s", num2str(conts(nSamples(1) ~= nSamples)));
+    warning("dhfun2:ft_read_header:NonMatchingNumberOfSamples", "Number of samples in CONT blocks of DH5 file don't match.\n\tExcluding CONT blocks:\n\t %s", num2str(conts(nSamples(1) ~= nSamples)));
     header.nSamples = mostCommonNSamples;
     conts = conts(iSelectedNSamples);
     nChanInCont = nChanInCont(iSelectedNSamples);
@@ -60,19 +87,25 @@ else
 end
 header.nSamplesPre = 0;
 nChannels = sum(nChanInCont);
+header.nChans = nChannels;
 
 %% channel labels
 header.label = cell(1, nChannels);
-header.chancount = cell(1, nChannels);
+header.chantype = cell(1, nChannels);
+header.chanunit = cell(1, nChannels);
+
 count = 1;
 for iCont = 1:length(conts)
     cont = conts(iCont);
     for iChan = 0:nChanInCont(iCont)-1
-        header.label{count} = sprintf('CONT%d/%02d', cont, iChan);
+        header.label{count} = sprintf('CONT%d/%d', cont, iChan);
         header.chantype{count} = 'lfp';
         header.chanunit{count} = 'V';
         count = count + 1;
     end
 end
+
+header.includedContIds = sort(conts);
+
 
 end
