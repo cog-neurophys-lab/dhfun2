986e29c2c9680dcb23ca987ce0aa17f1d39f4277
Joscha Schmiedt <schmiedt@uni-bremen.de>
2023-10-18 10:41:23 +0200

    Add calibration

--- +dh/ft_read_cont.m
+++ +dh/ft_read_cont.m
@@ -27,59 +27,68 @@ data.fsample = data.hdr.Fs;
 data.label = data.hdr.label;
 
 nTrialsInCont = dh.getcontindexsize(filename, blkid);
-if any(nTrialsInCont > 1)
-    % trial-based recording with gaps
-    if ~all(nTrialsInCont == nTrialsInCont(1))
-        error('dhfun2:ft_read_cont:NonMatchingNumberOfTrialsInCont', 'The number of trials stored in the selected CONT blocks don''t match');
-    end
+if ~all(nTrialsInCont == nTrialsInCont(1))
+    error('dhfun2:ft_read_cont:NonMatchingNumberOfTrialsInCont', ...
+        'The number of trials stored in the selected CONT blocks don''t match');
+end
+isTrialBasedRecording = any(nTrialsInCont > 1);
 
-    channelBegin = 1;
-    for iCont = 1:length(blkid)
+[nSamplesInCont, nChanInCont] = dh.getcontsize(filename, blkid);
 
-        contData = dh.readcont(filename, blkid(iCont));
-        [time, offset] = dh.readcontindex(filename, blkid(iCont));
-        [nSamplesInCont, nChanInCont] = dh.getcontsize(filename, blkid(iCont));
 
-        channelEnd = channelBegin + nChanInCont-1;
+channelBegin = 1;
+for iCont = 1:length(blkid)
 
-        for iTrial = 1:length(offset)-1
-            nSamples = offset(iTrial+1) - offset(iTrial);
-            data.trial{iTrial}(channelBegin:channelEnd,:) = contData(:,(offset(iTrial)+1):offset(iTrial+1));
+    % read data from disk
+    contData = double(dh.readcont(filename, blkid(iCont)));
+
+    % apply calibration
+    calibrationInfo = dh.getcontcalinfo(filename, blkid(iCont));
+    contData = contData .* repmat(calibrationInfo, [1, size(contData,2)]);
+    
+    % get dimensions of cont data
+
+    % determine end of channel range in data array
+    channelEnd = channelBegin + nChanInCont(iCont)-1;
+
+    % read start time and offset of all trials in this CONT block
+    [time, offset] = dh.readcontindex(filename, blkid(iCont));
+
+    if isTrialBasedRecording
+        if iCont == 1
+            data.trial = cell(1,nTrialsInCont(1));
+            data.time = cell(1,nTrialsInCont(1));
+        end
+
+        for iTrial = 1:length(offset)
+            if iTrial < length(offset)
+                nSamples = offset(iTrial+1) - offset(iTrial);
+                data.trial{iTrial}(channelBegin:channelEnd,:) = contData(:,(offset(iTrial)+1):offset(iTrial+1));
+            else
+                % last trial
+                nSamples = nSamplesInCont(iCont) - offset(end);
+                data.trial{iTrial}(channelBegin:channelEnd,:) = contData(:,offset(iTrial)+1:end);
+            end
+
             data.time{iTrial} = (0:nSamples-1) / data.hdr.Fs + time(iTrial)*1e-9;
             data.sampleinfo(iTrial, :) = double(round(time(iTrial) * 1e-9 * data.hdr.Fs) + [1 nSamples]);
         end
-
-        % last trial
-        iTrial = iTrial+1;
-        nSamples = nSamplesInCont - offset(end);
-        data.trial{iTrial}(channelBegin:channelEnd,:) = contData(:,offset(iTrial)+1:end);
-        data.time{iTrial} = (0:nSamples-1) / data.hdr.Fs + time(iTrial)*1e-9;
-        channelBegin = channelEnd+1;
-        data.sampleinfo(iTrial,:) = double(round(time(iTrial) * 1e-9 * data.hdr.Fs) + [1 nSamples]);
+        data.trialinfo = double([data.hdr.trialmap.TrialNo, data.hdr.trialmap.StimNo, data.hdr.trialmap.Outcome]);
+    else
+        % continuous recording / single trial
+        if iCont == 1
+            data.trial = {zeros(data.hdr.nChans, data.hdr.nSamples)};
+            data.time = {(0:data.hdr.nSamples-1)*data.hdr.Fs + data.hdr.FirstTimeStamp};
+            % sampleinfo: the begin and endsample of each trial relative to the recording on disk
+            data.sampleinfo = [1, data.hdr.nSamples];
+        end
+        data.trial{1}(channelBegin:channelEnd,:) = contData;        
     end
-    data.trialinfo = double([data.hdr.trialmap.TrialNo, data.hdr.trialmap.StimNo, data.hdr.trialmap.Outcome]);
 
-
-% single trial / continuous recording    
-else
-    % trials (load actual data)
-    data.trial = {zeros(data.hdr.nChans, data.hdr.nSamples)};
-    [~, nChanInCont] = dh.getcontsize(filename, blkid);
-    
-    channelBegin = 1;
-    for iCont = 1:length(blkid)
-        channelEnd = channelBegin + nChanInCont(iCont)-1;
-        data.trial{1}(channelBegin:channelEnd,:) = dh.readcont(filename, blkid(iCont));
-        channelBegin = channelEnd+1;
-    end
-    
-    % time
-    data.time = {(0:data.hdr.nSamples-1)*data.hdr.Fs + data.hdr.FirstTimeStamp};
-    
-    % sampleinfo: the begin and endsample of each trial relative to the recording on disk
-    data.sampleinfo = [1, data.hdr.nSamples];
-    
+    % move to next channel range
+    channelBegin = channelEnd+1;
 end
+
 
 
 % cfg
